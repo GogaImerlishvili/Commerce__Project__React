236c59d47bd0c37fa5247cf1525fcd1a9e4e3c9f
Goga <gogaimerlishvili1@gmail.com>
2022-03-29 22:40:27 +0400

    Added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import logo from "../assets/logo.svg";
-
+import { links } from "../utils/constants";
+import CartButtons from "./CartButtons";
 const Navbar = () => {
   return (
     <NavContainer>
@@ -15,13 +16,23 @@ const Navbar = () => {
           <FaBars />
         </button>
       </div>
-      <ul className="nav-links"></ul>
+      <ul className="nav-links">
+        {links.map((link) => {
+          const { id, text, url } = link;
+          return (
+            <li key={id}>
+              <Link to={url}>{text}</Link>
+            </li>
+          );
+        })}
+      </ul>
+      <CartButtons />
     </NavContainer>
   );
 };
 
 const NavContainer = styled.nav`
-  height: 5rem;
+  height: 6rem;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -29,7 +40,7 @@ const NavContainer = styled.nav`
   .nav-center {
     width: 100%;
     margin: 0 auto;
-    max-width: 1170px;
+    max-width: 100%;
     margin-left: 1%;
   }
 
@@ -38,7 +49,7 @@ const NavContainer = styled.nav`
     border: transparent;
     color: hsl(22, 31%, 52%);
     margin-left: 18%;
-    width: 100%;
+    /* width: 100%; */
     cursor: pointer;
     svg {
       font-size: 2rem;
@@ -49,9 +60,9 @@ const NavContainer = styled.nav`
     display: none;
   }
   @media (min-width: 992px) {
-    /* .nav-toggle {
+    .nav-toggle {
       display: none;
-    } */
+    }
     .nav-center {
       display: grid;
       grid-template-columns: auto 1fr auto;
@@ -61,15 +72,20 @@ const NavContainer = styled.nav`
     .nav-links {
       display: flex;
       justify-content: center;
+      margin-right: 30%;
+      text-decoration: none;
+      list-style: none;
+
       li {
-        margin: 0 0.5rem;
+        margin: 0 1.8rem;
       }
       a {
         color: grey;
-        font-size: 1rem;
+        font-size: 1.5rem;
         text-transform: capitalize;
         letter-spacing: 0.1rem;
         padding: 0.5rem;
+        text-decoration: none;
         &:hover {
           border-bottom: 2px solid #c5a491;
         }
