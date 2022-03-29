6f886fffee5e6340b1d8b72b2c37766e60b04e72
Goga <gogaimerlishvili1@gmail.com>
2022-03-29 15:15:39 +0400

    Added pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
 
-import { Home } from "./pages";
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
   return (
-    <div>
-      <h4>Comfy sloth starter</h4>
-      <Home />
-    </div>
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/products" element={<Products />} />
+        <Route exact path="/products/:id" element={<SingleProduct />} />
+        <Route exact path="/checkout" element={<Checkout />} />
+        <Route exact path="/*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
