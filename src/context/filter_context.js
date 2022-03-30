717e3b32d1f5d48937197f31764ce52a602f9eb2
Goga <gogaimerlishvili1@gmail.com>
2022-03-30 13:14:05 +0400

    Add products context

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,7 +1,30 @@
+import axios from "axios";
 import React, { useContext, useEffect, useReducer } from "react";
+import reducer from "../reducers/products_reducer";
+import { products_url as url } from "../utils/constants";
+import {
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+  GET_SINGLE_PRODUCT_BEGIN,
+  GET_SINGLE_PRODUCT_SUCCESS,
+  GET_SINGLE_PRODUCT_ERROR,
+} from "../utils/actions";
 
-const filter_context = () => {
-  return <div>filter_context</div>;
+const initialState = {};
+
+const ProductsContext = React.createContext();
+
+export const ProductsProvider = ({ children }) => {
+  return (
+    <ProductsContext.Provider value="products context">
+      {children}
+    </ProductsContext.Provider>
+  );
 };
 
-export default filter_context;
+export const useProductsContext = () => {
+  return useContext(ProductsContext);
+};
