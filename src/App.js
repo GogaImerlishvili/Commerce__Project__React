7c7c581579d407cd1bf0fdc95f19aa8fc619eda4
Goga <gogaimerlishvili1@gmail.com>
2022-04-11 04:31:33 +0400

    Added checkout

--- src/App.js
+++ src/App.js
@@ -23,7 +23,16 @@ function App() {
         <Route exact path="/cart" element={<Cart />} />
         <Route exact path="/products" element={<Products />} />
         <Route exact path="/products/:id" element={<SingleProduct />} />
-        <Route exact path="/checkout" element={<Checkout />} />
+
+        <Route
+          exact
+          path="/checkout/*"
+          element={
+            <PrivateRoute>
+              <Checkout />
+            </PrivateRoute>
+          }
+        />
         <Route exact path="/*" element={<Error />} />
       </Routes>
       <Footer />
